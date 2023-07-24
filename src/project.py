810df8934a2088947f9ee8d05701cbb134b8fcbd
Malini Agrawal <abcmalini@gmail.com>
2023-07-24 10:48:11 -0700

    update project file

--- src/project.py
+++ src/project.py
@@ -1,17 +1,54 @@
+import os
+import boto3
+import docx
 import qrcode
+from io import BytesIO
 
-# Define the path to your resume document file in the Windows file system
-resume_file_path = 'src/maliniresume-2023.docx'
+def generate_qr_code(event, context):
+    # Extract the file from the Lambda event
+    file_object = event['file']
 
-# Generate a QR code for the resume document file
-qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=4)
-qr.add_data(resume_file_path)
-qr.make(fit=True)
+    # Convert the binary file data to a Docx file
+    docx_file = BytesIO(file_object)
+    document = docx.Document(docx_file)
 
-# Create an image from the QR code
-qr_image = qr.make_image(fill_color="black", back_color="white")
+    # Extract text from the document
+    resume_text = ""
+    for paragraph in document.paragraphs:
+        resume_text += paragraph.text + "\n"
 
-# Save the QR code image
-qr_image.save('resume_qr_code.png')
+    # Generate the QR code
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(resume_text)
+    qr.make(fit=True)
 
-print("QR code generated successfully!")
+    qr_code_image = qr.make_image(fill_color="black", back_color="white")
+
+    # Save the QR code to a BytesIO object
+    qr_code_bytes_io = BytesIO()
+    qr_code_image.save(qr_code_bytes_io, format="PNG")
+    qr_code_bytes_io.seek(0)
+
+    # Upload the QR code image to an S3 bucket
+    s3_bucket_name = "your-s3-bucket-name"
+    qr_code_filename = "resume_qr_code.png"
+
+    s3_client = boto3.client('s3')
+    s3_client.put_object(
+        Bucket=s3_bucket_name,
+        Key=qr_code_filename,
+        Body=qr_code_bytes_io.read(),
+        ContentType="image/png"
+    )
+
+    # Return the S3 URL of the generated QR code
+    qr_code_url = f"https://{s3_bucket_name}.s3.amazonaws.com/{qr_code_filename}"
+    return {
+        "statusCode": 200,
+        "body": qr_code_url
+    }
